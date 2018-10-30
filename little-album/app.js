9a09aac2a1814a5efb24d85817d4fdb5b6b2727f
mf950511 <1099693871@qq.com>
2018-10-30 18:02:01 +0800

    day 04 end

--- little-album/app.js
+++ little-album/app.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express()
 var router = require('./controller/router')
+
+var bodyParser = require('body-parser')
+var urlencodedParser = bodyParser.urlencoded({extended: false})
 // 设置模板引擎
 app.set('view engine', 'ejs')
 
@@ -13,6 +16,12 @@ app.use(express.static('./uploads'))
 app.get('/', router.showIndex)
 
 app.get('/:albumName', router.showAlbum)
+
+app.get('/up', router.showUp)
+
+app.post('/up', router.doPost)
+
+app.post('/createFolder',urlencodedParser, router.createFodler)
 
 app.use((req, res) => {
   res.render('err')
--- little-album/controller/router.js
+++ little-album/controller/router.js
@@ -1,4 +1,8 @@
 var file = require('../models/file')
+var formidable = require('formidable')
+var path = require('path')
+var fs = require('fs')
+var sd = require('silly-datetime')
 // 首页
 exports.showIndex = (req ,res, next) =>{
   file.getAllAlbums((err, allAlbums)=>{
@@ -27,4 +31,63 @@ exports.showAlbum =  (req, res, next) => {
     })
   })
   
+}
+
+// 上传列表页
+exports.showUp = (req,res) => {
+  file.getAllAlbums((err, allAlbums) => {
+    res.render('up', {
+      albums: allAlbums
+    })
+  })
+  
+}
+
+// 上传文件
+exports.doPost = (req, res, next) => {
+  var form = new formidable.IncomingForm()
+  form.uploadDir = path.normalize(__dirname + '/../uploads/')
+  form.parse(req,(err, fileds, files) => {
+    console.log(fileds)
+    console.log(files)
+    if(err) {
+      next()
+      return
+    }
+    var size = files.img.size
+    if(size > 1024 * 2 * 1000) {
+      fs.unlink(files.img.path, () => {
+        
+      })
+      
+      res.send('图片尺寸应小于2M')
+      return
+    } 
+    var folder = fileds.folder
+    var oldPath = files.img.path
+    var ran = sd.format(new Date(), 'YYYYMMDDHHmm') + Math.round((Math.random() * 89999 + 10000))
+    var extname = path.extname(files.img.name)
+    var newPath = path.normalize(__dirname + '/../uploads/' + folder + '/' + ran + extname)
+    fs.rename(oldPath, newPath,(err) => {
+      if(err) {
+        res.send('改名失败')
+        return
+      }
+      res.send('上传成功')
+    })
+  })
+}
+
+// 新建文件夹
+exports.createFodler = (req, res) => {
+  var folderName = req.body.folderName
+  fs.mkdir(path.normalize(__dirname + '/../uploads/' + folderName), (err) => {
+    if(err) {
+      res.send('创建文件夹失败')
+      return
+    }
+    file.getAllAlbums((err, allAlbums) => {
+      res.json(allAlbums)
+    })
+  })
 }
